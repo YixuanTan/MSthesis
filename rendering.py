d0a553e7639a0b4ef8c45b8417a726b94ea901d8
YixuanTan <tanyx1990@gmail.com>
2017-05-06 01:58:13 -0400

    simple yet workable solution

--- rendering.py
+++ rendering.py
@@ -12,16 +12,6 @@ plt.ion()
 notifier = inotify.adapters.Inotify()
 notifier.add_watch('/home/smartcoder/Documents/Code/MS/ColumnarGrowth/')
 imgname = ""
-#fig = plt.figure()
-
-def plot_cont():    
-    def update(i):
-	    img = mpimg.imread(regx.sub('.png', imgname))
-	    imgplot = plt.imshow(img)
-
-    a = anim.FuncAnimation(fig, update, frames=100, repeat=False)
-    plt.show()
-    plt.pause(1)
 
 for event in notifier.event_gen():
     if event is not None:
@@ -29,10 +19,7 @@ for event in notifier.event_gen():
         if 'IN_CLOSE_WRITE' in event[1] and event[3].endswith(".dat"):
 		    #print "file '{0}' created in '{1}'".format(event[3], event[2])
 		    imgname = event[3]
-		    #time.sleep(0.1)
-		    response = subprocess.check_output("./mmsp2png " + imgname, shell=True, stderr=subprocess.STDOUT)
-		    #print response
-		    #plot_cont()
+		    subprocess.Popen("./mmsp2png " + imgname, shell=True, stderr=subprocess.STDOUT)
 		    #subprocess.check_call("rm *.dat", shell=True)
 
 
