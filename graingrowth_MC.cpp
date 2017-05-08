475e5e694b2fbf1dc94243e9925f33b7789c1497
YixuanTan <tanyx1990@gmail.com>
2017-05-08 09:05:26 -0400

    regular push

--- graingrowth_MC.cpp
+++ graingrowth_MC.cpp
@@ -80,7 +80,7 @@ namespace MMSP {
 	           		coords[0] = codx;
 	            	coords[1] = cody;
 	            //std::cout << grad_pos_start << " " << grad_pos_end << " " << maxTemp << " " << minTemp << std::endl;	
-		        if (codx > grad_pos_start && codx <= grad_pos_end) {
+		        if (codx >= grad_pos_start && codx <= grad_pos_end) {
 			    	grid.AccessToTmp(coords) = maxTemp - (maxTemp - minTemp) / (grad_pos_end - grad_pos_start) * (codx - grad_pos_start);
 			    } else {
 			    	grid.AccessToTmp(coords) = 273.0; // std::min(minTemp, 273.0);
@@ -1007,16 +1007,16 @@ template <int dim> unsigned long update(MMSP::grid<dim, unsigned long>& grid, in
 		ofs.close();
 
 		// smooth the gradient in the data to detect start and end 
-		int win_size = 10, curr_win_size = 0;
+		int win_size = 40, curr_win_size = 0;
 		int sum = 0;
 		double mostdense = 0.0, mostsparse = 1.0 * dim_x; 
 		double grains_along_line_global_smoothed[dim_x];
-		for(int i = 0; i < dim_x; i++) {
+		for(int i = dim_x - 1; i >= 0; i--) {
 			if(curr_win_size < win_size) {
 				curr_win_size++;
 				sum += grains_along_line_global[i];
 			} else {
-				sum -= grains_along_line_global[i - win_size];
+				sum -= grains_along_line_global[i + win_size];
 				sum += grains_along_line_global[i];
 			}
 			grains_along_line_global_smoothed[i] = 1.0 * sum / curr_win_size;
@@ -1025,24 +1025,47 @@ template <int dim> unsigned long update(MMSP::grid<dim, unsigned long>& grid, in
 		}
 
 		// detect
-		for(int i = 0; i < dim_x; i++) {
-			if(grains_along_line_global_smoothed[i] > 1.1 * mostsparse) {
+		for(int i = 0; i < dim_x - win_size; i++) {
+			
+			if(grains_along_line_global_smoothed[i] > mostsparse + 0.1 * (mostdense - mostsparse)) {
+				//grad_pos_start = max(grad_pos_start, i);
 				grad_pos_start = i;
 				break;
 			}
+			/*
+			if(grains_along_line_global_smoothed[i] == mostsparse) {
+				grad_pos_start = i; 
+				break;
+			}
+			*/
 		}
-		for(int i = dim_x - 1; i >= 0; i--) {
-			if(grains_along_line_global_smoothed[i] < 0.90 * mostdense) {
+		for(int i = dim_x - 1 - win_size; i >= 0; i--) {
+			
+			if(grains_along_line_global_smoothed[i] < mostdense - 0.2 * (mostdense - mostsparse)) {
+				//grad_pos_end = min(grad_pos_end, i);
 				grad_pos_end = i;
 				break;
 			}
+			/*
+			if(grains_along_line_global_smoothed[i] == mostdense) {
+				grad_pos_end = i; 
+				break;
+			}
+			*/
 		}
-
+		/*
+		// randomly shift the range by one
+		if(double(rand())/double(RAND_MAX) < 0.5) {
+			grad_pos_start++;
+			grad_pos_end++; 
+		}
+		*/
+		std::cout << mostsparse << " " << mostdense << " " << grad_pos_start << " " << grad_pos_end << std::endl;
 		ofs.open("size_history_smooth.txt", std::ofstream::out | std::ofstream::app);	
 		for(int i = 0; i < dim_x; i++) {
 			ofs << grains_along_line_global_smoothed[i] << " ";
 		}
-		ofs << "\n";
+		ofs << "\n\n";
 		ofs.close();
 
 	}
